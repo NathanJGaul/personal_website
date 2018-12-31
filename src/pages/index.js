4594e5547eea724fa593a5219582c2b3c54339c9
NathanJG <nathanjgaul@gmail.com>
2018-12-31 13:53:41 -0700

    Out with the old

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
-
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <div>
+      <h1>nathanjgaul</h1>
+      <div>
+        <Link>About</Link> | <Link>Contact</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  </div>
+  );
 
-export default IndexPage
+export default IndexPage;
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from 'typography';
+import stowTheme from 'typography-theme-stow-lake';
+
+const typography = new Typography(stowTheme);
+
+export default typography;
